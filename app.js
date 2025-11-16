0e35c5d6c4387be4e94cf8d067c09d80a55ffcf3
CordovaPaulo <202311378@gordoncollege.edu.ph>
2025-11-16 18:58:36 +0800

    cors fix

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(cookieParser());
 
 // CORS configuration - MUST BE BEFORE ROUTES
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    allowedOrigin: process.env.FRONTEND_URL,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
